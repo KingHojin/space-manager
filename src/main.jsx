408aaa8b2b32f8fb89977b1f5098981430b00b18
KingHojin <84673753+KingHojin@users.noreply.github.com>
2026-07-10 19:56:46 +0900

    Load quiet UI overrides after existing styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles.css";
 import "./crewMotion.css";
 import "./uiPolish.css";
+import "./appleUI.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
